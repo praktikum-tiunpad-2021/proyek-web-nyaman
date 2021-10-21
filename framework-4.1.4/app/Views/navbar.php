9b2ae8b17449e8bcb6afbbabcb3ec1594719155c
kharisma14 <kharismanisa14@gmail.com>
2021-10-21 21:24:24 +0700

    benerin home + rapihin

---
+++ framework-4.1.4/app/Views/navbar.php
@@ -0,0 +1,9 @@
+<div class="topnav">
+    <div class="logo">
+        <img src="assets/img/logoterang.png" alt="nyaman" width="100%">
+    </div>
+    <a href="#">Beranda</a></li>
+    <a href="#">Reservasi</a></li>
+    <a href="#">Login</a></li>
+    <div class="clearfix"></div>
+</div>
